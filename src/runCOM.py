b2ca9b38ebd01d739c75cbab9dd9ec54657fa616
UnaiCaja <unaicaja@gmail.com>
2023-08-14 20:34:22 +0200

    Added plot of convex hull

--- src/runCOM.py
+++ src/runCOM.py
@@ -8,23 +8,34 @@ com = mesh.center_mass
 
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_trisurf(mesh.vertices[:, 0],
+ax1 = fig.add_subplot(121, projection='3d')
+ax1.plot_trisurf(mesh.vertices[:, 0],
                 mesh.vertices[:,1],
                 triangles=mesh.faces,
                 Z=mesh.vertices[:,2],
                 alpha=0.5) 
-ax.axis("equal")
-ax.scatter(com[0],com[1],com[2],color="r")
+ax1.axis("equal")
+ax1.title.set_text('COM of mesh')
+ax1.scatter(com[0],com[1],com[2],color="r")
+
+
+# Get the convex hull
+convex_hull = mesh.convex_hull
+com_ch = convex_hull.center_mass
+# You can also visualize the convex hull
+
+ax2 = fig.add_subplot(122, projection='3d')
+ax2.plot_trisurf(convex_hull.vertices[:, 0],
+                convex_hull.vertices[:,1],
+                triangles=convex_hull.faces,
+                Z=convex_hull.vertices[:,2],
+                alpha=0.5) 
+ax2.axis("equal")
+ax2.title.set_text('COM of convex hull of mesh')
+ax2.scatter(com_ch[0],com_ch[1],com_ch[2],color="r")
 
 
 
-
-
-# # Get the convex hull
-# convex_hull = mesh.convex_hull
-# # You can also visualize the convex hull
-# convex_hull.show()
 
 
 plt.show()
