0877b624f07cbe19ed3c6076e76b8fba4653bb3b
UnaiCaja <unaicaja@gmail.com>
2023-10-03 20:59:48 +0200

    Rigged hand

--- src/runMagicTrick.py
+++ src/runMagicTrick.py
@@ -1,83 +1,249 @@
+import bpy, bmesh, mathutils
+import math
 import trimesh
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib import cm
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection # used for 3D visualization 
-import utils
-import os, shutil, json
+import numpy as np, scipy as sp
 
+mesh_name = "Grimm"
+armature_name = "Armature"
 
-w=2; wch = 1
-def make_plot(mesh,mesh_name="",plot_path: str = None):
-    # Makes a plot of a particular mesh and finds the "best" vertex to do the balancing trick
-    dp = utils.compute_stability_forall_vertices(mesh)
-    dp_ch = utils.compute_stability_forall_vertices(mesh,use_com_of_ch=True)
-    idx = np.nanargmax(w*dp-wch*dp_ch)
+# From stackoverflow (for printing into console here)
+def print(data):
+    for window in bpy.context.window_manager.windows:
+        screen = window.screen
+        for area in screen.areas:
+            if area.type == 'CONSOLE':
+                override = {'window': window, 'screen': screen, 'area': area}
+                bpy.ops.console.scrollback_append(override, text=str(data), type="OUTPUT")    
 
-    com = mesh.center_mass
-    com_ch = mesh.convex_hull.center_mass
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111, projection='3d')
-    ax1.plot_trisurf(mesh.vertices[:, 0],
-                    mesh.vertices[:,1],
-                    triangles=mesh.faces,
-                    Z=mesh.vertices[:,2],
-                    alpha=0.1) 
-    ax1.axis("equal")
-    ax1.axis("off")
-    ax1.title.set_text(f'Mesh {mesh_name} point {idx}')
-    vertex = mesh.vertices[idx,:]
-    # Pto_plot = np.array([com,com_ch,vertex])
-    ax1.scatter(*com,marker="o",label="com",color="b")
-    ax1.scatter(*com_ch,marker="s",label="com_ch",color="k")
-    ax1.scatter(*vertex,marker="*",label="vertex",color="r")
-    ax1.legend()
+#################################################
 
-    if plot_path == None:
-        plt.show()
-    else:
-        plt.savefig(plot_path)
+# Get the original and deformed meshes
+mesh = bpy.data.objects[mesh_name]  # (basically just needed for local to global coord matrix)
+def get_deformed_mesh():
+    depsgraph = bpy.context.evaluated_depsgraph_get()
+    return mesh.evaluated_get(depsgraph).to_mesh()
+#################################################
+# Define energy (objective) function
+def energy():
+    deformed_mesh = get_deformed_mesh()
+    local_nose = (deformed_mesh.vertices[48].co + deformed_mesh.vertices[66].co) / 2.0
+    world_nose = mesh.matrix_world @ local_nose
+    return (target.location - world_nose).length
+#################################################
+# Depth first search through bones
+def dfs_bone(bone,v_idx,mean=0.01,dev=0.005,reg_coef = 0.01):
+    '''Coordinate descent for each of the bone angles'''
+    
+    step_size = np.random.normal(loc=mean,scale=dev)
+    # Save the initial angles for regularization purposes
+    try:
+        bone["initial_angles"] = bone["initial_angles"]
+    except:
+        bone["initial_angles"] = [bone.rotation_euler[i] for i in range(3)]
+    
+    bone.rotation_mode = 'XYZ'
+    for i in range(3):
+        # Compute energies for each update choice
+        old_angle = bone.rotation_euler[i]
+        old_energy = objective_function(v_idx)
+        # Make update and copute new energies
+        bone.rotation_euler[i] += step_size
+        plus_angle = bone.rotation_euler[i]
+        plus_reg = (bone["initial_angles"][i] - plus_angle)**2*reg_coef
+        plus_energy = objective_function(v_idx) + plus_reg
+        bone.rotation_euler[i] -= 2 * step_size
+        minus_angle = bone.rotation_euler[i]
+        minus_reg = (bone["initial_angles"][i] - minus_angle)**2*reg_coef
+        minus_energy = objective_function(v_idx) + minus_reg
+        # Select energy minimizing choice
+        if plus_energy < min(old_energy, minus_energy):
+            bone.rotation_euler[i] = plus_angle
+        elif minus_energy < max(old_energy, plus_energy):
+            bone.rotation_euler[i] = minus_angle
+        else:
+            bone.rotation_euler[i] = old_angle
+            
+#        if bone.name == 'top_fin':
+#            print(('old angle & energy:', old_angle, old_energy))
+#            print(('new angle & energy:', bone.rotation_euler[i], energy()))
+    # Recurse
+    for child in bone.children:
+        dfs_bone(child,v_idx)
+#################################################
 
-# def remesh_dataset(dataset_path,new_dir):
-#     # Remesh every object in a folder
-#     for filename in os.listdir(dataset_path):
-#         if filename == ".DS_Store":
-#             continue
+def approx_grad_descent(bone,v_idx,step,reg_coef = 0.01):
+    '''Does one gradient descent step on each angle for the bone'''
+    
+    # Save the initial angles for regularization purposes
+    try:
+        bone["initial_angles"] = bone["initial_angles"]
+    except:
+        bone["initial_angles"] = [bone.rotation_euler[i] for i in range(3)]
+    
+    bone.rotation_mode = 'XYZ'
+    for i in range(3):
+        # Compute energies at two points
+        bone.rotation_euler[i] += step
+        plus_angle = bone.rotation_euler[i]
+        plus_reg = (bone["initial_angles"][i] - plus_angle)**2*reg_coef
+        plus_energy = objective_function(v_idx) + plus_reg
+        # Repeat
+        bone.rotation_euler[i] -= 2 * step
+        minus_angle = bone.rotation_euler[i]
+        minus_reg = (bone["initial_angles"][i] - minus_angle)**2*reg_coef
+        minus_energy = objective_function(v_idx) + minus_reg
+        # Set the angle back to its original value
+        bone.rotation_euler[i] += step
+        # Approximate the derivative at that point
+        deriv = (plus_energy - minus_energy)/(2*step)
+        #Update the angle
+        bone.rotation_euler[i] += step*deriv   
+    # Recurse
+    for child in bone.children:
+        dfs_bone(child,v_idx)
 
-#         mesh_path = os.path.join(dataset_path,filename)
-#         ms = pymeshlab.MeshSet()
-#         ms.load_new_mesh(mesh_path)
-#         ms.meshing_isotropic_explicit_remeshing(adaptive=True,targetlen=pymeshlab.Percentage(0.1))
-#         new_mesh_path = os.path.join(new_dir,filename)
-#         ms.save_current_mesh(new_mesh_path)
+#################################################
+def get_trimesh_object():
+    # Convert mesh to bmesh
+    dmesh = get_deformed_mesh()
+    bm = bmesh.new()
+    bm.from_mesh(dmesh)
+    # Get the coordinates of all vertices
+    M = mesh.matrix_world
+    V = np.array([M @ v.co for v in bm.verts])
+    # Get face list
+    F = [
+    [v.index for v in f.verts]
+    for f in bm.faces
+    ]
+    # Get the face normals
+    N = [np.array(f.normal) for f in bm.faces]
+    
+    return trimesh.Trimesh(vertices=V,faces=F,face_normals=N)
 
-def write_mesh_info(mesh,text_file,mesh_name="",diff_bound=0.3):
-    # Add line to a txt file with information for that particular mesh
-    dp = utils.compute_stability_forall_vertices(mesh)
-    dp_ch = utils.compute_stability_forall_vertices(mesh,use_com_of_ch=True)
-    # w=2; wch = 1
-    v_idx = np.nanargmax(w*dp-wch*dp_ch)
-    dp_r = np.round(dp[v_idx],decimals=2) 
-    dp_ch_r = np.round(dp_ch[v_idx],decimals=2)
-    diff = np.round(dp_r-dp_ch_r,decimals=2)
-    if diff < diff_bound:
-        return
-    with open(text_file,"a") as f:
-        f.write(f"Mesh {mesh_name} -- at v_idx={v_idx} we have dp={dp_r}, dp_ch={dp_ch_r} with diff {diff}\n")
+#################################################
 
-def rotate_mesh(mesh):
-    # Find special vertex
-    dp = utils.compute_stability_forall_vertices(mesh)
-    dp_ch = utils.compute_stability_forall_vertices(mesh,use_com_of_ch=True)
-    w=2; wch = 1
-    v_idx = np.nanargmax(w*dp-wch*dp_ch)
-    vertex = mesh.vertices[v_idx]
-    # Find vectors that you want to rotate
-    com = mesh.center_mass
-    v1 = com - vertex
-    v1 = v1/np.linalg.norm(v1)
-    v2 = np.array([0,-1,0])
-    # Find rotation that takes v1 into v2
+def compute_dot_products(v_idx):
+    tmesh = get_trimesh_object()
+    COM = tmesh.center_mass
+    COM_CH = tmesh.convex_hull.center_mass
+    # Get the vertex normal
+    vertex_normal = tmesh.vertex_normals[v_idx]
+    vertex_normal = vertex_normal / np.linalg.norm(vertex_normal)
+    # Vectors going to com and com_ch
+    vertex_to_com = COM - tmesh.vertices[v_idx]
+    vertex_to_com = vertex_to_com/np.linalg.norm(vertex_to_com)
+    vertex_to_com_ch = COM_CH - tmesh.vertices[v_idx]
+    vertex_to_com_ch = vertex_to_com_ch/np.linalg.norm(vertex_to_com_ch)
+    # Compute the dot produtcs
+    dp = np.dot(vertex_to_com,vertex_normal)
+    dp_ch = np.dot(vertex_to_com_ch,vertex_normal)
+    return dp, dp_ch
+#################################################
+
+def compute_all_dot_products():
+    tmesh = get_trimesh_object()
+    COM = tmesh.center_mass
+    COM_CH = tmesh.convex_hull.center_mass
+    # Get the vertex normal
+    vertex_normals = tmesh.vertex_normals
+    norm = np.linalg.norm(vertex_normals, axis=1)[:, None]
+    vertex_normals = vertex_normals/norm
+    # Vectors going to com and com_ch
+    vertex_to_com = COM - tmesh.vertices
+    norm = np.linalg.norm(vertex_to_com, axis=1)[:, None]
+    vertex_to_com = vertex_to_com/norm
+    vertex_to_com_ch = COM_CH - tmesh.vertices
+    norm = np.linalg.norm(vertex_to_com_ch, axis=1)[:, None]
+    vertex_to_com_ch = vertex_to_com_ch/norm
+    # Compute the dot produtcs
+    # Dot products
+    dp = np.einsum('ij,ij->i', vertex_normals,vertex_to_com)
+    dp_ch = np.einsum('ij,ij->i', vertex_normals,vertex_to_com_ch)
+    return dp, dp_ch
+
+#################################################
+
+def find_best_index(w1=2,w2=1):
+    dp, dp_ch = compute_all_dot_products()
+    return np.argmax(w1*dp - w2*dp_ch)
+
+#################################################
+def objective_function(v_idx,w1 = 2, w2 = 1):  
+    dp, dp_ch = compute_dot_products(v_idx)
+    # Return the weighted difference
+    return -(w1*dp - w2*dp_ch)
+
+#################################################
+def show_vertex(v_idx,ball_size=0.003):
+    """Creates an icosahedron at the location of a point"""
+    # Get the mesh
+    dmesh = get_deformed_mesh()
+    bm = bmesh.new()
+    bm.from_mesh(dmesh)
+    # Get the coordinates of the vertex
+    bm.verts.ensure_lookup_table()
+    point = mesh.matrix_world @ bm.verts[v_idx].co
+    # Draw the icosahedron
+    bpy.ops.mesh.primitive_ico_sphere_add(radius=1,
+                                        enter_editmode=False, 
+                                        align='WORLD', 
+                                        location=point,
+                                        scale=(ball_size,ball_size,ball_size))
+
+#################################################
+def show_com(ball_size=0.003):
+    """Creates an icosahedron at the location of a point"""
+    # Get the mesh
+    tmesh = get_trimesh_object()
+    COM = tmesh.center_mass
+    # Draw the icosahedron
+    bpy.ops.mesh.primitive_ico_sphere_add(radius=1,
+                                        enter_editmode=False, 
+                                        align='WORLD', 
+                                        location=COM,
+                                        scale=(ball_size,ball_size,ball_size))
+
+#################################################
+def run_optimization(epochs,step,reg_coef,v_idx=None,delta=0.0001):
+    # Get the armature & target
+    armature = bpy.data.objects.get(armature_name)
+    # Set mode to pose + get bone (assuming armature is selected)
+    bpy.ops.object.mode_set(mode='POSE')
+    # Select vertex for the optimization
+    if v_idx == None:
+        v_idx = find_best_index()
+    print("The chosen index is {idx}".format(idx=v_idx))
+    dp_best,dp_ch_best = compute_dot_products(v_idx)
+    print("With values dp={a}, dp_ch={b}".format(a=dp_best,b=dp_ch_best))
+    # Print initial values of dot products
+    dp, dp_ch = compute_dot_products(v_idx)
+    print("Initial values dp:{a}, dp_ch:{b}".format(a=dp,b=dp_ch))
+    # Run the optimzation loop
+    for i in range(epochs):
+        old_energy = objective_function(v_idx)
+        for bone in armature.pose.bones:
+            if not bone.parent:
+                approx_grad_descent(bone,v_idx,step,reg_coef)
+        # Termination criteria
+        if objective_function(v_idx) - old_energy > delta:
+            print('stopped after %i epochs' % i)
+            break
+    # Print final values of dot products
+    dp, dp_ch = compute_dot_products(v_idx)
+    print("Final values dp:{a}, dp_ch:{b}".format(a=dp,b=dp_ch))
+    # Update the scene to reflect the changes
+    bpy.context.scene.frame_set(bpy.context.scene.frame_current)
+    return v_idx
+
+#################################################
+def find_rotation_matrix(v1,v2,p):
+    """
+    Computes the matrix of a rotation with origin at p.
+    The transforation maps v1 to v2 and is stored as a 4x4 matrix.
+    v1 and v2 are assumed to be unit vectors.
+    """
+    
     w = np.cross(v1,v2)
     M = np.array([
         [0,-w[2],w[1]],
@@ -86,146 +252,21 @@ def rotate_mesh(mesh):
     ])
     c = np.dot(v1,v2)
     R = np.eye(3) + M + M@M/(1+c) #Rotation that takes v1 to v2
-    # Transform the vertices appropriately
-    mesh.vertices = vertex + (mesh.vertices - vertex) @ np.transpose(R)
-
-# def get_shapenet_dirs(name: str):
-#     """Given a common name, finds the list of directories associated with it"""
-
-#     result = []
-#     with open("ShapeNetCore.v2/taxonomy.json","r") as f:
-#         data = json.load(f)
-#         for synset in data:
-#             if name in synset["name"]:
-#                 path = f"ShapeNetCore.v2/{synset['synsetId']}"
-
-def as_mesh(scene_or_mesh):
-    """
-    Convert a possible scene to a mesh.
-
-    If conversion occurs, the returned mesh has only vertex and face data.
-    """
-    if isinstance(scene_or_mesh, trimesh.Scene):
-        if len(scene_or_mesh.geometry) == 0:
-            mesh = None  # empty scene
-        else:
-            # we lose texture information here
-            mesh = trimesh.util.concatenate(
-                tuple(trimesh.Trimesh(vertices=g.vertices, faces=g.faces)
-                    for g in scene_or_mesh.geometry.values()))
-    else:
-        assert(isinstance(scene_or_mesh, trimesh.Trimesh))
-        mesh = scene_or_mesh
-    return mesh
-
-            
-################################################################################################################
-
-# mesh_path = "ShapeNetCore.v2/02691156/64dbeac5250de2bfc5d50480246d2c40" # Airplane
-# mesh_path = "ShapeNetCore.v2/03001627/1c685bc2a93f87a2504721639e19f609" #Chair1
-# mesh_path = "ShapeNetCore.v2/03001627/212e266cab46f1dd5b903ba10d2ec446" #Chair2
-# mesh_path = "/Users/unaicaja/Desktop/airplane.obj" # Remeshed airplane
-# mesh_path = "ShapeNetCore.v2/03046257/929afe0b3ad23608c7b37fe5d21f2385"  # Clock
-mesh_path = "ShapeNetCore.v2/03636649/a21744a28956c93cc5a208307819a3a1" #Lamp1
-# mesh_path = "ShapeNetCore.v2/03636649/763ceeaa4ce2ce1d6afaa22adc38077c" #Lamp2
-# mesh_path = "ShapeNetCore.v2/03636649/8425fcfab0cd9e601f0d826a92e0299f" #Lamp3
-
-
-mesh_path = os.path.join(mesh_path,"models/model_normalized.obj")
-mesh = trimesh.load_mesh(mesh_path)
-mesh = as_mesh(mesh)
-# write_mesh_info(mesh,"ShapeNet_results/summary.txt","Remeshed airplane",diff_bound=0)
-make_plot(mesh)
-
-
-################################################################################################################
-# # SEARCHING A SYNSET IN SHAPENET
-# name = "bed"
-# f = open("ShapeNetCore.v2/taxonomy.json","r")
-# data = json.load(f)
-# # Build a map with keys being all directories to study and values being
-# dirs_to_study = []
-# for synset in data:
-#     if not name in synset["name"]:
-#         continue
+    b = p - R@p
+    T = np.ones((4,4))
+    T[0:3,0:3] = R
+    T[3,0:3] = b
+    T[0:3,3] = b
     
-#     id = synset['synsetId']
-#     path = f"ShapeNetCore.v2/{id}"
-#     dirs_to_study.append(path)
-# f.close()
+    return mathutils.Matrix(T)
 
-# count = 0
-# for path in dirs_to_study:
-#     if not os.path.isdir(path):
-#         print(f"CAREFUL: DIRECTORY {path} DOES NOT EXIST")
-#         continue
+    
+    
 
-#     for id in os.listdir(path):
-#         dir = os.path.join(path,id)
-#         if not os.path.isdir(dir):
-#             continue
-        
-#         mesh_path = os.path.join(dir,"models/model_normalized.obj")
-#         scene_or_mesh = trimesh.load_mesh(mesh_path)
-#         mesh = as_mesh(scene_or_mesh)
+#################################################
 
-#         if mesh is None:
-#             print(f"{dir} seems to have no geometry")
-#             continue
-
-#         print(f"Is {dir} watertight: {mesh.is_watertight}")
-#         if not mesh.is_watertight:
-#             continue
-        
-#         text_file = "ShapeNet_results/summary.txt"
-#         write_mesh_info(mesh=mesh,mesh_name=dir,text_file=text_file)
-        
-
-################################################################################################################
-# TRY TO MAKE COLORFUL PLOT
-# dp = utils.compute_stability_forall_vertices(mesh)
-# colors = plt.cm.RdYlGn(dp)
-# mesh.visual = trimesh.visual.color.ColorVisuals(mesh=mesh,vertex_colors=colors)
-# x=mesh.show(
-#     smooth=True
-# )
-
-# mesh_path = f"fixed_hands/hand_29.obj"
-# mesh = trimesh.load_mesh(mesh_path)
-# rotate_mesh(mesh)
-# new_mesh_path = f"meshes_to_render/hand_29.obj"
-# mesh.export(new_mesh_path)
-# trimesh.export(mesh,new_mesh_path)
-
-################################################################################################################
-# LOOKING FOR INTERESTING POINTS IN DATASET
-
-# dataset_path = "/Users/mac/Documents/GitHub/magic-physics-illusions/shrek_dataset/test_normal/test_normal"
-# dataset_path = "meshes_to_examine"
-# text_file = "results/shrek1.txt"
-# mesh_names = os.listdir(dataset_path)
-# differences = np.zeros(len(mesh_names))
-# for i,filename in enumerate(mesh_names):
-#     if filename == ".DS_Store":
-#         continue
-
-#     mesh_path = os.path.join(dataset_path,filename)
-#     mesh = trimesh.load_mesh(mesh_path)
-#     dp = utils.compute_stability_forall_vertices(mesh)
-#     dp_ch = utils.compute_stability_forall_vertices(mesh,use_com_of_ch=True)
-#     v_idx = np.argmax(dp-dp_ch)
-#     differences[i] = dp[v_idx] - dp_ch[v_idx]
-
-# for i in range(len(mesh_names)):
-#     if differences[i] <= 0.3 or dp[i] <= 0.5:
-#         continue
-
-#     mesh_path = os.path.join(dataset_path,mesh_names[i])
-#     mesh = trimesh.load_mesh(mesh_path)
-#     make_plot(mesh,i,mesh_names[i])#,plot_path=f"results/{mesh_names[i]}_idx{i}.png"
-
-
-
-
-
-
+epochs = 20;step = 0.2;reg_coef = 1
+v_idx = 8457
+v_idx = run_optimization(epochs,step,reg_coef,v_idx)
+show_vertex(v_idx)
+show_com()
